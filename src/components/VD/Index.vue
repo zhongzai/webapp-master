107e7f68d5f648abf5a0144d1b69e7cb8e90ec30
= <932598485@qq.com>
2017-10-23 10:22:38 +0800

    提交

---
+++ src/components/VD/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view style="height: 100%"></router-view>
+</template>
+<script>
+
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/VD/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+  import { Navbar, TabItem } from 'mint-ui';
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
